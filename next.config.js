b2908ab84b7e267973639db0a1d4b8e6ffaae6d5
Steve Brand <thatfanficguy@protonmail.com>
2023-04-30 23:53:38 +0200

    Add rewrites to config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
 	experimental: {
 		appDir: true,
 	},
+	async rewrites() {
+		return [
+			{
+				source: '/subscribe',
+				destination: 'https://0z5gh.mjt.lu/wgt/0z5gh/z2t/subscribe?c=8214db8c',
+			},
+		]
+	},
 };
 
 module.exports = nextConfig;
