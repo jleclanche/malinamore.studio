27f6cdc0b41c9006d79bd92b2fa1a1b30dc11f0f
Steve Brand <thatfanficguy@protonmail.com>
2023-05-08 19:31:34 +0200

    Remove reduntant style

--- app/about/team/page.tsx
+++ app/about/team/page.tsx
@@ -32,7 +32,7 @@ const people = [
 ];
 
 const TeamPage = () => (
-	<div className="bg-white py-32">
+	<div className="py-32">
 		<div className="mx-auto max-w-7xl px-6 text-center lg:px-8">
 			<div className="mx-auto max-w-2xl">
 				<h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
