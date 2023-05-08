fc3b96d95e696664f081d1469b506f8ff2345394
Steve Brand <thatfanficguy@protonmail.com>
2023-05-08 21:42:27 +0200

    Formatting

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
 	async rewrites() {
 		return [
 			{
-				source: '/subscribe',
-				destination: 'https://0z5gh.mjt.lu/wgt/0z5gh/z2t/subscribe?c=8214db8c',
+				source: "/subscribe",
+				destination: "https://0z5gh.mjt.lu/wgt/0z5gh/z2t/subscribe?c=8214db8c",
 			},
 		]
 	},
