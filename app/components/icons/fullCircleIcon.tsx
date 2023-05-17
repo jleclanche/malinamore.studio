261aa3bd5f1af4301ac8d886f505f5053e5e6108
Steve Brand <thatfanficguy@protonmail.com>
2023-05-17 21:03:10 +0200

    Add inline icons

---
+++ app/components/icons/fullCircleIcon.tsx
@@ -0,0 +1,7 @@
+const fullCircleIcon = () => (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="145.6 75.6 408.8 408.8">
+		<path d="M350 75.6A204.6 204.6 0 0 0 145.6 280c0 112.7 91.7 204.4 204.4 204.4S554.4 392.7 554.4 280 462.7 75.6 350 75.6z" />
+	</svg>
+);
+
+export default fullCircleIcon;
---
+++ app/components/icons/temperatureIcon.tsx
@@ -0,0 +1,16 @@
+const temperatureIcon = () => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"
+		viewBox="0 0 24 24"
+	>
+		<path stroke="none" d="M0 0h24v24H0z" />
+		<path d="M10 13.5a4 4 0 1 0 4 0V5a2 2 0 0 0-4 0v8.5M10 9h4" />
+	</svg>
+);
+
+export default temperatureIcon;
