12088c88a4ab860b27ab77e70305ae76ef5233f0
Jerome Leclanche <jerome@leclan.ch>
2023-05-27 00:55:54 +0200

    Update phone number

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
 export const CONTACT_EMAIL = "contact@malinamore.studio";
-export const CONTACT_PHONE = "+32 494 646278";
+export const CONTACT_PHONE = "+32 2 894 63 38";
 export const CALENDAR_LINK = "https://cal.com/malina-more";
 export const GA_MEASUREMENT_ID = "G-JYM4MSTWKP";
