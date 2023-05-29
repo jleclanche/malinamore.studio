1ac512d8f43aa70c0aa023e757d1532cc70d188e
Jerome Leclanche <jerome@leclan.ch>
2023-05-29 20:36:12 +0200

    Fix GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,29 @@
 import { Inter } from "next/font/google";
 import Footer from "./Footer";
 import Header from "./Header";
-import GoogleAnalytics from "./components/GoogleAnalytics";
+import { GA_MEASUREMENT_ID } from "./constants";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const GA_CODE = `
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+	dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "${GA_MEASUREMENT_ID}", { page_path: window.location.pathname });
+`;
+
+const GoogleAnalytics = () => (
+	<>
+		<script
+			async
+			src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+		/>
+		<script id="google-analytics" dangerouslySetInnerHTML={{ __html: GA_CODE }} />
+	</>
+);
 
 export const metadata = {
 	title: "Malina More Studio",
@@ -20,7 +39,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang={lang}>
-			<GoogleAnalytics />
+			<head>
+				<GoogleAnalytics />
+			</head>
 			<body className={`${inter.className} relative min-h-screen`}>
 				<Header />
 				<div>{children}</div>
