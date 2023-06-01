bb54f4a2c4e3ebd5d96dc4d96664cd95bff9ac59
Jerome Leclanche <jerome@leclan.ch>
2023-06-01 15:59:38 +0200

    Add json-ld

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { LocalBusiness, WithContext } from "schema-dts";
 import Footer from "./Footer";
 import Header from "./Header";
 import { GA_MEASUREMENT_ID } from "./constants";
@@ -27,7 +28,41 @@ const GoogleAnalytics = () => (
 
 export const metadata = {
 	title: "Malina More Studio",
-	description: "Art & ceramics studio in Brussels",
+	description: "A beautiful 180m2 ceramics, arts & crafts studio in Brussels.",
+};
+
+const jsonLd: WithContext<LocalBusiness> = {
+	"@context": "https://schema.org",
+	"@type": "LocalBusiness",
+	name: "Malina More Studio",
+	address: {
+		"@type": "PostalAddress",
+		streetAddress: "Rue Rossini 19",
+		addressLocality: "Anderlecht",
+		addressRegion: "Brussels",
+		postalCode: "1070",
+		addressCountry: "Belgium",
+	},
+	url: "https://www.malinamore.studio",
+	email: "contact@malinamore.studio",
+	telephone: "+32 2 894 63 38",
+	logo: "https://malinamore.studio/icon.png",
+	description: metadata.description,
+	smokingAllowed: false,
+	sameAs: [
+		"https://www.instagram.com/malina_more_studio",
+		"https://www.facebook.com/MalinaMoreStudio",
+	],
+	parentOrganization: {
+		"@type": "Organization",
+		name: "Malina More ASBL",
+		legalName: "Malina More ASBL",
+		nonprofitStatus: "NonprofitSBBI",
+		url: "https://malinamore.be",
+		email: "contact@malinamore.be",
+		foundingDate: "2023-04-01",
+		vatID: "BE0800279001",
+	},
 };
 
 export default function RootLayout({
@@ -46,6 +81,10 @@ export default function RootLayout({
 				<Header />
 				<div>{children}</div>
 				<Footer />
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+				/>
 			</body>
 		</html>
 	);
