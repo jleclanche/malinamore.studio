45a7c0f87507a76de3d9d33c6e5f11a3577fde16
Jerome Leclanche <jerome@leclan.ch>
2023-06-02 21:54:03 +0200

    Add redirect links

--- next.config.js
+++ next.config.js
@@ -12,6 +12,18 @@ const nextConfig = {
 				source: "/subscribe",
 				destination: "https://0z5gh.mjt.lu/wgt/0z5gh/z2t/subscribe?c=8214db8c",
 			},
+			{
+				source: "/r/meetup",
+				destination: "https://www.meetup.com/Malina-More/",
+			},
+			{
+				source: "/r/facebook",
+				destination: "https://facebook.com/MalinaMoreStudio",
+			},
+			{
+				source: "/r/instagram",
+				destination: "https://instagram.com/malina_more_studio",
+			},
 		];
 	},
 	experimental: {
