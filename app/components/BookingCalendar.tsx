445fcda0940b4373fc343371a3cba4c817f6602c
Jerome Leclanche <jerome@leclan.ch>
2023-08-10 15:41:57 +0200

    Remove cal.com

--- app/components/BookingCalendar.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-// @ts-expect-error (no types that we can find)
-import Cal from "@calcom/embed-react";
-
-const BookingCalendar = () => <Cal calLink="malina-more" />;
-
-export default BookingCalendar;
